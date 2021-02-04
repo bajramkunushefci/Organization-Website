f2a0dca90dc44b649d0abf821462296aea85de9c
bajramkunushefci <bk50883@universum-ks.org>
2021-02-04 23:52:44 +0100

    Changes

--- OrganizationWebsite/main.js
+++ OrganizationWebsite/main.js
@@ -22,3 +22,30 @@
 //       }; // End if
 //     });
 // });
+
+const hamburger = document.querySelector('.siteWrapper .site-header .nav-list .hamburger');
+const mobile_menu = document.querySelector('.siteWrapper .site-header .nav-list ul');
+const menu_item = document.querySelectorAll('.siteWrapper .site-header .nav-list ul li a');
+const header = document.querySelector('.site-header');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 50) {
+        header.style.backgroundColor = "#292826";
+    }
+    else {
+        header.style.backgroundColor = "transparent";
+    }
+});
+
+menu_item.forEach(item=>{
+    item.addEventListener('click',()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
