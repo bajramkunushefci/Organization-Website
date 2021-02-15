31cb8c5009f46643c9de4d85a103b79f8088e34e
bajramkunushefci <bk50883@universum-ks.org>
2021-02-15 13:14:48 +0100

    Changes

--- OrganizationWebsite/main.js
+++ OrganizationWebsite/main.js
@@ -1,28 +1,3 @@
-// $(document).ready(function(){
-//     // Add smooth scrolling to all links
-//     $("a").on('click', function(event) {
-  
-//       // Make sure this.hash has a value before overriding default behavior
-//       if (this.hash !== "") {
-//         // Prevent default anchor click behavior
-//         event.preventDefault();
-  
-//         // Store hash
-//         var hash = this.hash;
-  
-//         // Using jQuery's animate() method to add smooth page scroll
-//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//         $('html, body').animate({
-//                 scrollTop: $(hash).offset().top
-//             }, 800, function(){
-     
-//           // Add hash (#) to URL when done scrolling (default click behavior)
-//           window.location.hash = hash;
-//         });
-//       }; // End if
-//     });
-// });
-
 const hamburger = document.querySelector('.siteWrapper .site-header .nav-list .hamburger');
 const mobile_menu = document.querySelector('.siteWrapper .site-header .nav-list ul');
 const menu_item = document.querySelectorAll('.siteWrapper .site-header .nav-list ul li a');
